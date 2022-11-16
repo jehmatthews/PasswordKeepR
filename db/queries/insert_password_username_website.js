072d60b67e70cfa9ec96387499dece26eb68fd16
DionKanhai <dionkanhais@gmail.com>
2022-11-16 16:51:21 +0000

    add comments

--- db/queries/insert_password_username_website.js
+++ db/queries/insert_password_username_website.js
@@ -1,6 +1,7 @@
 // establish the connection to the database
 const db = require('../connection');
 
+// function that uses a query and promises to insert into the database
 const addNewPassword = function(password) {
   db.query(`INSERT INTO passwords (websiteName, category, urlName, username)
   VALUES ($1, $2, $3, $4) RETURNING *`, [password.websiteName, password.category, password.urlName, password.username])
